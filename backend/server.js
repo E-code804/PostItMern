f6c1f6358a7fb5b8d2d532a913dfc33858ba44cc
E-code804 <erikpfeffer5@gmail.com>
2023-08-14 14:49:59 -0400

    Preparing for vercel deployment 3.

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,14 @@ const postRoutes = require("./routes/posts");
 // Middleware
 app.use(express.json()); // This looks for if the request has data in it, and if it does it will attach to the req variable.
 //app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", process.env.URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", process.env.URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use((req, res, next) => {
   // This func fires for every request that comes in.
   // Must call the "next" function in order to move onto next piece of middleware.
@@ -42,6 +43,6 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  // res.setHeader("Access-Control-Allow-Credential", "true");
+  res.setHeader("Access-Control-Allow-Credential", "true");
   res.json("Connected to Backend!");
 });
