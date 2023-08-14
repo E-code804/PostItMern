71922c05fe88bb5fffa4720051c2e653d2af3432
E-code804 <erikpfeffer5@gmail.com>
2023-08-14 14:03:29 -0400

    Preparing for vercel deployment.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const postRoutes = require("./routes/posts");
 // Middleware
 app.use(express.json()); // This looks for if the request has data in it, and if it does it will attach to the req variable.
 //app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use((req, res, next) => {
   // This func fires for every request that comes in.
   // Must call the "next" function in order to move onto next piece of middleware.
