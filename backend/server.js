56d8bc67d6699307b98d931658cd9e65c05c2783
E-code804 <erikpfeffer5@gmail.com>
2023-08-14 14:41:52 -0400

    Preparing for vercel deployment 3.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json()); // This looks for if the request has data in it, and if
 //app.use(bodyParser.json());
 app.use(
   cors({
-    origin: process.env.URL,
+    origin: ["http://localhost:3000", process.env.URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -27,24 +27,21 @@ app.use((req, res, next) => {
 app.use("/api/posts", postRoutes);
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI);
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     // Listening for requests at a certain port
-//     const PORT = process.env.PORT || 4000;
-//     app.listen(PORT, () => {
-//       console.log("Connected to DB and listening on port", PORT);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+// mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listening for requests at a certain port
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log("Connected to DB and listening on port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
+  // res.setHeader("Access-Control-Allow-Credential", "true");
   res.json("Connected to Backend!");
-});
-
-app.listen(4000, () => {
-  console.log("Connected to DB and listening on port 4000");
 });
