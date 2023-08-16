b27fcc00688c0d46d827b41707ab246c785423d7
E-code804 <erikpfeffer5@gmail.com>
2023-08-16 00:32:49 -0400

    Fixed more styles, added skeleton loading to EditTopic form, and adjusted url.

--- client/src/backendURL.js
+++ client/src/backendURL.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:4000";
-//export const url = "https://postitapi-2o1p.onrender.com";
+//export const url = "http://localhost:4000";
+export const url = "https://postitapi-2o1p.onrender.com";
